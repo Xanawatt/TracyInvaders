d908ec5927933a1e824452994c3a01e88ddc8ad8
Xanawatt <schneidermark2000@gmail.com>
2017-04-27 09:19:57 -0400

    This is the thing

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -13,6 +13,7 @@ import org.newdawn.slick.state.StateBasedGame;
 
 import characters.LargeInvader;
 import characters.Player;
+import characters.SmallInvader;
 import main.Main;
 
 public class GameState extends BasicGameState {
@@ -24,48 +25,63 @@ public class GameState extends BasicGameState {
 
 	public static final int LARGE_INVADER1_X = 250;
 	public static final int LARGE_INVADER1_Y = 250;
-	
+
 	public static final int LARGE_INVADER2_X = 500;
 	public static final int LARGE_INVADER2_Y = 250;
-	
-	public static final int[] LARGE_INVADER_X = {250, 500};
-	public static final int[] LARGE_INVADER_Y = {250, 250};
-	
+
+	public static final int[] LARGE_INVADER_X = { 250, 500 };
+	public static final int[] LARGE_INVADER_Y = { 250, 250 };
+	public static LargeInvader[] largeInvader = new LargeInvader[10];
+	public static SmallInvader[] smallInvader = new SmallInvader[10];
+
 	public static int playerScore = 0;
-	
+
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		//tracyImage = new Image("textures/tracyDepot2/largeTracy1.png");
+		// tracyImage = new Image("textures/tracyDepot2/largeTracy1.png");
 		player = new Player(playerXPosition, playerYPosition);
-		largeInvader0 = new LargeInvader(LARGE_INVADER_X[0], LARGE_INVADER_Y[0]);
-		largeInvader1 = new LargeInvader(LARGE_INVADER_Y[1], LARGE_INVADER_Y[1]);
-		/*for (int i = 0; i < 30) {
-		 * 
-			sdfgh
-		}*/
+		/*largeInvader0 = new LargeInvader(LARGE_INVADER_X[0], LARGE_INVADER_Y[0]);
+		largeInvader1 = new LargeInvader(LARGE_INVADER_Y[1], LARGE_INVADER_Y[1]);*/
+		int xStart = 100;
+		int yStart = 250;
+		for (int i = 0; i < largeInvader.length; i++) {
+			largeInvader[i] = new LargeInvader(xStart, yStart);
+			xStart += 100;
+		}
+		xStart = 100;
+		yStart = 400;
+		for (int i = 0; i < smallInvader.length; i++) {
+			smallInvader[i] = new SmallInvader(xStart, yStart);
+			xStart += 100;
+		}
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		player.move(gc, g);
 		player.shoot(gc, g);
-		if (largeInvader0.isDead == true) {
-			largeInvader0.largeInvaderAnimation.stop();
-		} else {
-			largeInvader0.animate(gc, g, LARGE_INVADER1_X, LARGE_INVADER1_Y);
+		for (int i = 0; i < largeInvader.length; i++) {
+			if (largeInvader[i].isDead == true) {
+				largeInvader[i].largeInvaderAnimation.stop();
+			} else {
+				largeInvader[i].animate(gc, g, largeInvader[i].getX(), largeInvader[i].getY());
+			}
+			g.drawString(Integer.toString(playerScore), 640, 10);
 		}
-		if (largeInvader1.isDead == true) {
-			largeInvader1.largeInvaderAnimation.stop();
-		} else {
-			largeInvader1.animate(gc, g, LARGE_INVADER2_X, LARGE_INVADER2_Y);
+		
+		for (int i = 0; i < smallInvader.length; i++) {
+			if (smallInvader[i].isDead == true) {
+				smallInvader[i].smallInvaderAnimation.stop();
+			} else {
+				smallInvader[i].animate(gc, g, smallInvader[i].getX(), smallInvader[i].getY());
+			}
+			g.drawString(Integer.toString(playerScore), 640, 10);
 		}
-		g.drawString(Integer.toString(playerScore), 640, 10);
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-		
-		
+
 	}
 
 	@Override
