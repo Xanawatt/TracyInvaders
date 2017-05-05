7044d665075ea707db5232c96ecd8ad14b6e964c
Xanawatt <schneidermark2000@gmail.com>
2017-05-05 10:10:11 -0400

    added more invaders, but they don't move down correctly

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -37,135 +37,187 @@ public class GameState extends BasicGameState {
 	public static final int[] LARGE_INVADER_Y = { 250, 250 };
 	public static LargeInvader[] largeInvaderRow1 = new LargeInvader[10];
 	public static LargeInvader[] largeInvaderRow2 = new LargeInvader[10];
-	public static LargeInvader[] largeInvaderRow3 = new LargeInvader[10];
-	public static SmallInvader[] smallInvader = new SmallInvader[10];
+	public static SmallInvader[] smallInvaderRow1 = new SmallInvader[10];
+	public static SmallInvader[] smallInvaderRow2 = new SmallInvader[10];
+	public static SmallInvader[] smallInvaderRow3 = new SmallInvader[10];
+
+	public static LargeInvader[][] largeInvader = new LargeInvader[2][10];
+	public static SmallInvader[][] smallInvader = new SmallInvader[3][10];
 
 	public static int playerScore = 0;
 	public static boolean exit = false;
+
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		// tracyImage = new Image("textures/tracyDepot2/largeTracy1.png");
 		player = new Player(playerXPosition, playerYPosition);
-		ufo = new UFO(0,150);
+		ufo = new UFO(0, 150);
+
 		int xStart = 100;
-		int yStart = 100;
-		for (int i = 0; i < largeInvaderRow1.length; i++) {
-			largeInvaderRow1[i] = new LargeInvader(xStart, yStart);
-			xStart += 100;
+		int yStart = 200;
+		for (int i = 0; i < largeInvader.length; i++) {
+			for (int j = 0; j < largeInvader[i].length; j++) {
+				largeInvader[i][j] = new LargeInvader(xStart, yStart);
+				xStart += 100;
+			}
+			xStart = 100;
+			yStart += 50;
 		}
-		for (int i = 0; i < largeInvaderRow2.length; i++) {
-			largeInvaderRow2[i] = new LargeInvader(xStart, yStart);
-			xStart += 100;
-			yStart += 100;
-		}
-		for (int i = 0; i < largeInvaderRow3.length; i++) {
-			largeInvaderRow3[i] = new LargeInvader(xStart, yStart);
-			xStart += 100;
-			yStart += 100;
-		}
-		
+
 		xStart = 100;
-		yStart = 400;
+		yStart = 300;
 		for (int i = 0; i < smallInvader.length; i++) {
-			smallInvader[i] = new SmallInvader(xStart, yStart);
-			xStart += 100;
+			for (int j = 0; j < smallInvader[i].length; j++) {
+				smallInvader[i][j] = new SmallInvader(xStart, yStart);
+				xStart += 100;
+			}
+			xStart = 100;
+			yStart += 50;
+
 		}
-		
+
+		/*
+		 * for (int i = 0; i < largeInvaderRow1.length; i++) {
+		 * largeInvaderRow1[i] = new LargeInvader(xStart, yStart); xStart +=
+		 * 100; }
+		 * 
+		 * yStart += 100; for (int i = 0; i < largeInvaderRow2.length; i++) {
+		 * largeInvaderRow2[i] = new LargeInvader(xStart, yStart); xStart +=
+		 * 100;
+		 * 
+		 * }
+		 */
+
+		/*
+		 * xStart = 100; yStart = 400; for (int i = 0; i <
+		 * smallInvaderRow1.length; i++) { smallInvaderRow1[i] = new
+		 * SmallInvader(xStart, yStart); xStart += 100; } yStart += 100; for
+		 * (int i = 0; i < smallInvaderRow2.length; i++) { smallInvaderRow2[i] =
+		 * new SmallInvader(xStart, yStart); xStart += 100; } yStart+= 100; for
+		 * (int i = 0; i < smallInvaderRow3.length; i++) { smallInvaderRow3[i] =
+		 * new SmallInvader(xStart, yStart); xStart += 100; }
+		 */
+
 	}
 
 	float loops = 0;
-	float xTrans = 0.2f;
+	float xTrans = 0.5f;
 	String direction = "right";
+
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		g.drawString("" + playerScore, 600, 0);
-		if(UFO.countTicks == true){
+		if (UFO.countTicks == true) {
 			ticks++;
 		}
-		
-		if(UFO.isDead == true){
+
+		if (UFO.isDead == true) {
 			deathTicks++;
 		}
 		player.move(gc, g);
 		player.shoot(gc, g);
 		player.checkForCollisions(gc, g);
 		UFO.move(gc, g);
-//		for (int i = 0; i < largeAndSmallInvaders.length; i++) {
-//			for (int j = 0; j < largeAndSmallInvaders )
-//		}
-		
-		for (int i = 0; i < largeInvaderRow1.length; i++) {
-			if (largeInvaderRow1[i].isDead == true) {
-				largeInvaderRow1[i].largeInvaderAnimation.stop();
-			} else {
-				largeInvaderRow1[i].tryToShoot(gc, g);
-				
-				if (direction.equals("right")) {
-					if (largeInvaderRow1[i].getX() > 1100) {
-						for (int j = 0; j < largeInvaderRow1.length; j++) {
-							largeInvaderRow1[j].animate(gc, g, largeInvaderRow1[j].getX() - 1, largeInvaderRow1[j].getY() + 50);
-							smallInvader[j].animate(gc, g, smallInvader[j].getX() - 1, smallInvader[j].getY() + 50);
+
+		for (int i = 0; i < largeInvader.length; i++) {
+			for (int j = 0; j < largeInvader[i].length; j++) {
+				if (largeInvader[i][j].isDead == true) {
+					largeInvader[i][j].largeInvaderAnimation.stop();
+				} else {
+					largeInvader[i][j].tryToShoot(gc, g);
+					if (direction.equals("right")) {
+						if (largeInvader[i][j].getX() > 1100) {
+							for (int k = 0; k < largeInvader.length; k++) {
+								for (int l = 0; l < largeInvader[k].length; l++) {
+									largeInvader[k][l].animate(gc, g, largeInvader[k][l].getX() - 1,
+											largeInvader[k][l].getY() + 50);
+								}
+							}
+							for (int k = 0; k < smallInvader.length; k++) {
+								for (int l = 0; l < smallInvader[k].length; l++) {
+									smallInvader[k][l].animate(gc, g, smallInvader[k][l].getX() - 1,
+											smallInvader[k][l].getY() + 50);
+								}
+							}
+							direction = "left";
+						} else {
+							largeInvader[i][j].animate(gc, g, largeInvader[i][j].getX() + xTrans,
+									largeInvader[i][j].getY());
+
 						}
-						direction = "left";
-					} else {
-						largeInvaderRow1[i].animate(gc, g, largeInvaderRow1[i].getX() + xTrans, largeInvaderRow1[i].getY());
-						
-					}
-				} else if (direction.equals("left")) {
-					if (largeInvaderRow1[i].getX() < 100) {
-						for (int j = 0; j < largeInvaderRow1.length; j++) {
-							largeInvaderRow1[j].animate(gc, g, largeInvaderRow1[j].getX() + 1, largeInvaderRow1[j].getY() + 50);
-							smallInvader[j].animate(gc, g, smallInvader[j].getX() + 1, smallInvader[j].getY() + 50);
+					} else if (direction.equals("left")) {
+						if (largeInvader[i][j].getX() < 100) {
+							for (int k = 0; k < largeInvader.length; k++) {
+								for (int l = 0; l < largeInvader[k].length; l++) {
+									largeInvader[k][l].animate(gc, g, largeInvader[k][l].getX() + 1,
+											largeInvader[k][l].getY() + 50);
+								}
+							}
+							for (int k = 0; k < smallInvader.length; k++) {
+								for (int l = 0; l < smallInvader[k].length; l++) {
+									smallInvader[k][l].animate(gc, g, smallInvader[k][j].getX() + 1,
+											smallInvader[k][l].getY() + 50);
+								}
+							}
+							direction = "right";
+						} else {
+							largeInvader[i][j].animate(gc, g, largeInvader[i][j].getX() - xTrans,
+									largeInvader[i][j].getY());
 						}
-						direction = "right";
-					} else {
-						largeInvaderRow1[i].animate(gc, g, largeInvaderRow1[i].getX() - xTrans, largeInvaderRow1[i].getY());
 					}
 				}
 			}
 		}
-		
 
 		for (int i = 0; i < smallInvader.length; i++) {
-			if (smallInvader[i].isDead == true) {
-				smallInvader[i].smallInvaderAnimation.stop();
-			} else {
-				
-				if (largeInvaderRow1[i].getY() >= Main.GAME_HEIGHT) {
-					exit = true;
-				}
-				if (direction.equals("right")) {
-					if (smallInvader[i].getX() > 1100) {
-						for (int j = 0; j < smallInvader.length; j++) {
-							largeInvaderRow1[j].animate(gc, g, largeInvaderRow1[j].getX() - 1, largeInvaderRow1[j].getY() + 50);
-							smallInvader[j].animate(gc, g, smallInvader[j].getX() - 1, smallInvader[j].getY() + 50);
+			for (int j = 0; j < smallInvader[i].length; j++) {
+				if (smallInvader[i][j].isDead == true) {
+					smallInvader[i][j].smallInvaderAnimation.stop();
+				} else {
+					// smallInvader[i][j].tryToShoot(gc, g);
+					if (direction.equals("right")) {
+						if (smallInvader[i][j].getX() > 1100) {
+							for (int k = 0; k < smallInvader.length; k++) {
+								for (int l = 0; l < smallInvader[k].length; l++) {
+									smallInvader[k][l].animate(gc, g, smallInvader[k][l].getX() - 1,
+											smallInvader[k][l].getY() + 50);
+								}
+							}
+							for (int k = 0; k < largeInvader.length; k++) {
+								for (int l = 0; l < largeInvader[k].length; l++) {
+									largeInvader[k][l].animate(gc, g, largeInvader[k][l].getX() - 1,
+											largeInvader[k][l].getY() + 50);
+								}
+							}
+							direction = "left";
+						} else {
+							smallInvader[i][j].animate(gc, g, smallInvader[i][j].getX() + xTrans,
+									smallInvader[i][j].getY());
+
 						}
-						direction = "left";
-					} else {
-						smallInvader[i].animate(gc, g, smallInvader[i].getX() + xTrans, smallInvader[i].getY());
-					}
-				} else if (direction.equals("left")) {
-					if (smallInvader[i].getX() < 100) {
-						for (int j = 0; j < smallInvader.length; j++) {
-							largeInvaderRow1[j].animate(gc, g, largeInvaderRow1[j].getX() + 1, largeInvaderRow1[j].getY() + 50);
-							smallInvader[j].animate(gc, g, smallInvader[j].getX() + 1, smallInvader[j].getY() + 50);
+					} else if (direction.equals("left")) {
+						if (smallInvader[i][j].getX() < 100) {
+							for (int k = 0; k < smallInvader.length; k++) {
+								for (int l = 0; l < smallInvader[k].length; l++) {
+									smallInvader[k][l].animate(gc, g, smallInvader[k][l].getX() + 1,
+											smallInvader[k][l].getY() + 50);
+								}
+							}
+							for (int k = 0; k < largeInvader.length; k++) {
+								for (int l = 0; l < largeInvader[k].length; l++) {
+									largeInvader[k][l].animate(gc, g, largeInvader[k][j].getX() + 1,
+											largeInvader[k][l].getY() + 50);
+								}
+							}
+							direction = "right";
+						} else {
+							smallInvader[i][j].animate(gc, g, smallInvader[i][j].getX() - xTrans,
+									smallInvader[i][j].getY());
 						}
-						direction = "right";
-					} else {
-						smallInvader[i].animate(gc, g, smallInvader[i].getX() - xTrans, smallInvader[i].getY());
 					}
 				}
 			}
 		}
-		/*
-		 * if (largeInvader[9].getX() > 1100 && largeInvader[9].getY() == 250) {
-		 * for (int i = 0; i < largeInvader.length; i++) {
-		 * largeInvader[i].animate(gc, g, largeInvader[i].getX(),
-		 * largeInvader[i].getY() + 50); } } else {
-		 */
-
-		// }
-		loops += 1f;
 	}
 
 	@Override
