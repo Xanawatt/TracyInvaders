c626ee3297944d84d5b29b56fae70b7a2a4f2362
Xanawatt <schneidermark2000@gmail.com>
2017-05-11 09:15:58 -0400

    Merge remote-tracking branch 'refs/remotes/origin/garrett-branch'
    # Conflicts:
    #	src/states/GameState.java

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -51,10 +51,10 @@ public class GameState extends BasicGameState {
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		// tracyImage = new Image("textures/tracyDepot2/largeTracy1.png");
 		player = new Player(playerXPosition, playerYPosition);
-		ufo = new UFO(0, 150);
+		ufo = new UFO(0, 50);
 
 		int xStart = 100;
-		int yStart = 200;
+		int yStart = 100;
 		for (int i = 0; i < largeInvader.length; i++) {
 			for (int j = 0; j < largeInvader[i].length; j++) {
 				largeInvader[i][j] = new LargeInvader(xStart, yStart);
@@ -64,14 +64,14 @@ public class GameState extends BasicGameState {
 			yStart += 50;
 		}
 
-		xStart = 100;
-		yStart = 300;
+		xStart = 108;
+		yStart = 200;
 		for (int i = 0; i < smallInvader.length; i++) {
 			for (int j = 0; j < smallInvader[i].length; j++) {
 				smallInvader[i][j] = new SmallInvader(xStart, yStart);
 				xStart += 100;
 			}
-			xStart = 100;
+			xStart = 108;
 			yStart += 50;
 
 		}
